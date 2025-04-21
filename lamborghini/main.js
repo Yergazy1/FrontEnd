657688861cc320cb720c8a7bcf7c7ff9a7f01fba
Yergazy <153651469+Yergazy1@users.noreply.github.com>
2025-04-21 23:21:05 +0500

    Add files via upload

---
+++ lamborghini/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView:2,
+    loop: true,
+    spaceBetween:20,
+  
+    
+  
+    navigation: {
+      nextEl: '.swiper-button-right',
+      prevEl: '.swiper-button-left',
+    },
+  
+    
+  });
